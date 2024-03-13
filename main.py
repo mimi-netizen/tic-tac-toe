594dedafe0431297297d9d963dc487fa40b04039
Celyne <dianakydd2@gmail.com>
2024-03-13 17:05:37 +0300

    updated info

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from helpers import draw_board
 spots = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5',
          6:'6', 7:'7', 8:'8', 9:'9'}
 
-draw_board
+playing = True
+
+while playing:
+    draw_board(spots)
+    # Get input from the player
+    choice = input()
+    if choice == 'q':
+        playing = False
