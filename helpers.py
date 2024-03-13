529696698acdf86a0d3b0718e0d8ee4a7fd2c2af
Celyne <dianakydd2@gmail.com>
2024-03-13 17:34:59 +0300

    Updated helpers

--- helpers.py
+++ helpers.py
@@ -7,3 +7,23 @@ def draw_board(spots):
     def check_turn(turn):
         if turn % 2 == 0: return 'O'
         else: return 'X'
+
+        def check_for_win(spots):
+        #Handle Horizontal cases
+          if    (spots[1] == spots[2] == spots[3]) \
+             or (spots[4] == spots[5] == spots[6]) \
+             or (spots[7] == spots[8] == spots[9]):
+             return True
+          
+        # Handle Vertical cases
+          elif    (spots[1] == spots[4] == spots[7]) \
+             or (spots[2] == spots[5] == spots[8]) \
+             or (spots[3] == spots[6] == spots[9]):
+             return True
+          
+        #Handle Diagonal cases
+          elif    (spots[1] == spots[5] == spots[9]) \
+             or (spots[3] == spots[5] == spots[7]):
+             return True
+          
+          else: return False
