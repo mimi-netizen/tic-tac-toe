b39d11d49c8490d69a702c85c07f84f61437813b
Celyne <dianakydd2@gmail.com>
2024-03-13 17:27:48 +0300

    Removes bugs

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ spots = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5',
 
 playing = True
 turn = 0
+prev_turn = -1
 
 while playing:
     # Reset the screen
     os.system('cls' if os.name == 'nt' else 'clear')
     draw_board(spots)
+    # If an invalid turn occurs, let player know
+    if prev_turn == turn:
+        print("Invalid spot selected, please pick another.")
+    prev_turn = turn
     print("Player" + str((turn % 2) + 1) + "'s turn: Pick your spot or press q tp quit")
     # Get input from the player
     choice = input()
